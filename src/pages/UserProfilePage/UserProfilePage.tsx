0f51feebc11440a2e5c3332da56c4228e232440d
Sereda Vitaliy <sereda.v484@gmail.com>
2026-06-28 12:11:15 +0200

    fix

--- src/pages/UserProfilePage/UserProfilePage.tsx
+++ src/pages/UserProfilePage/UserProfilePage.tsx
@@ -1,5 +1,145 @@
-import UserProfile from "@/components/UserProfile/UserProfile";
+import { getUser, updateUser } from "@/api/AdminApi";
+import { User, UserRequest } from "@/types/admin";
+import { notification, Button, Form, Input } from "antd";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { FIELD_MAX_LENGTH, USERNAME_MIN_LENGTH } from "@/constants/validation";
+import styles from "./UserProfilePage.module.css";
 
 export default function UserProfilePage() {
-  return <UserProfile />;
+  const { id } = useParams();
+
+  const [user, setUser] = useState<User | null>(null);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [form] = Form.useForm();
+
+  const navigate = useNavigate();
+  useEffect(
+    function () {
+      async function fetchUser() {
+        try {
+          const data = await getUser(Number(id));
+          setUser(data);
+          form.setFieldsValue({
+            username: data.username,
+            email: data.email,
+            phoneNumber: data.phoneNumber,
+          });
+        } catch (error) {
+          notification.error({
+            message: "не удалось получить данные пользователя!",
+          });
+        }
+      }
+      fetchUser();
+    },
+    [id],
+  );
+
+  async function handleSaveClick(values: UserRequest) {
+    if (!user) return;
+
+    const updatedData: UserRequest = {};
+
+    let key: keyof UserRequest;
+    for (key in values) {
+      if (values[key] !== user[key]) {
+        updatedData[key] = values[key];
+      }
+    }
+
+    try {
+      const update = await updateUser(Number(id), updatedData);
+      setUser(update);
+      setIsEditing(false);
+    } catch (error) {
+      notification.error({
+        message: "не удалось обновить данные пользователя!",
+      });
+    }
+  }
+
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <Button type="primary" onClick={() => navigate("/users")}>
+          назад
+        </Button>
+        <div className={styles.titleBlock}>
+          <h1 className={styles.title}>Профиль пользователя</h1>
+          <p className={styles.subtitle}>{user?.username}</p>
+        </div>
+      </header>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={handleSaveClick}
+        id="profileForm"
+      >
+        <Form.Item
+          label="имя пользователя"
+          name="username"
+          rules={[
+            {
+              whitespace: true,
+            },
+            {
+              pattern: /^[a-zA-Zа-яёА-ЯЁ\s]+$/,
+              message: "Вводить можно только символы латиницы или кириллицы",
+            },
+            {
+              min: USERNAME_MIN_LENGTH,
+              message: `Минимальное количество символов: ${USERNAME_MIN_LENGTH}`,
+            },
+            {
+              max: FIELD_MAX_LENGTH,
+              message: `Максимальное количество символов: ${FIELD_MAX_LENGTH}`,
+            },
+          ]}
+        >
+          <Input disabled={!isEditing} />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "Введите верный E-mail!",
+            },
+          ]}
+        >
+          <Input disabled={!isEditing} />
+        </Form.Item>
+        <Form.Item
+          label="номер телефона"
+          name="phoneNumber"
+          rules={[
+            {
+              pattern: /^(\+|7|8)\d{9,15}$/,
+              message: "введите корректный номер телефона",
+            },
+          ]}
+        >
+          <Input disabled={!isEditing} />
+        </Form.Item>
+      </Form>
+      <div className={styles.actions}>
+        {isEditing ? (
+          <Button
+            type="primary"
+            htmlType="submit"
+            form="profileForm"
+            key="save"
+          >
+            Сохранить
+          </Button>
+        ) : (
+          <Button type="primary" onClick={() => setIsEditing(true)} key="edit">
+            Редактировать
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
